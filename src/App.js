a279360487e09e74d918c2edc401a7daeaecc63c
almarpr4 <almarpr4@server.fake>
2020-06-27 22:57:54 +0200

    One movie shown in the show PLP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <section className="jumbotron text-center">
+          <div className="container">
+            <h1 className="jumbotron-heading">Album example</h1>
+            <p className="lead text-muted">Something short and leading about the collection below—its contents, the
+              creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+            <p>
+              <a href="#" className="btn btn-primary my-2">Main call to action</a>
+              <a href="#" className="btn btn-secondary my-2">Secondary action</a>
+            </p>
+          </div>
+        </section>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card mb-4 box-shadow">
+              <img className="card-img-top" data-src=""
+                   src="http://static.tvmaze.com/uploads/images/medium_portrait/31/78286.jpg" alt="Card image cap" />
+                <div className="card-body">
+                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.</p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small className="text-muted">9 mins</small>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
