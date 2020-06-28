e2f082e2e14b525b0f83391b67f16debaf1ee515
almarpr4 <almarpr4@server.fake>
2020-06-28 23:24:10 +0200

    Fix codesandbox kink

--- src/components/Header/HeaderComponent.test.js
+++ src/components/Header/HeaderComponent.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, fireEvent, Simulate } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import HeaderComponent from './HeaderComponent';
 
 test('renders Header Component without crashing', () => {
