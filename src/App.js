699251b8822204b8c771de97b3d7271768bac29c
almarpr4 <almarpr4@server.fake>
2020-06-28 12:47:46 +0200

    Add route for pdp and plp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import ShowListingPage from "./components/PLP/ShowListingPage";
 import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ShowListingPage from './components/PLP/ShowListingPage';
 import showData from './movies.json'
+import ShowDetailPage from "./components/PDP/ShowDetailPage";
 
-function App() {
+export default function BasicExample() {
   return (
-    <div className="App">
-      <ShowListingPage shows={showData} />
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <ShowListingPage shows={showData} />
+          </Route>
+          <Route path="/detail">
+            <ShowDetailPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
