68f30d87c78f22fb5fd34db037d47c70a87b371b
almarpr4 <almarpr4@server.fake>
2020-06-28 14:26:57 +0200

    Navigate from PLP to PDP

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import ShowListingPage from "./components/PLP/ShowListingPage";
 import 'bootstrap/dist/css/bootstrap.css';
--- src/components/PLP/ShowTile.js
+++ src/components/PLP/ShowTile.js
@@ -1,20 +1,24 @@
 import React from "react";
+import {
+  Link
+} from "react-router-dom";
 
-
-function ShowTile({ name, premiered, rating, image }) {
+function ShowTile({id, name, premiered, rating, image}) {
   return (
     <div className="col-md-4">
+      <Link to={`/detail/${id}`}>
       <div className="card mb-4 box-shadow">
-        <img className="card-img-top" data-src=""
-             src={image.medium} alt="Card image cap" />
-        <div className="card-body">
-          <div className="justify-content-center align-items-center">{name}</div>
-          <div className="d-flex justify-content-between align-items-center">
-            <small className="text-muted">Year: {premiered}</small>
-            <small className="text-muted">Rating: {rating.average}</small>
+          <img className="card-img-top" data-src=""
+               src={image.medium} alt="Card image cap"/>
+          <div className="card-body">
+            <div className="justify-content-center align-items-center">{name}</div>
+            <div className="d-flex justify-content-between align-items-center">
+              <small className="text-muted">Year: {premiered}</small>
+              <small className="text-muted">Rating: {rating.average}</small>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
 
   );
